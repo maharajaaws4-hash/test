8c8d07f611c020bc5932854d1e301ac8a45b20a4
aws maharaja <maharajaaws4@gmail.com>
2026-04-22 02:31:15 +0530

    Add Terraform config and GitHub Actions workflow

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
-iprovider "aws" {
+provider "aws" {
   region = "ap-south-1"
 }
 
-# Create a new key pair using the repo file
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file("${path.module}/key/id_rsa.pub")
-}
-
-# Create a new security group
 resource "aws_security_group" "my_sg" {
   name        = "my-sg"
   description = "Allow SSH and HTTP"
@@ -35,7 +28,6 @@ resource "aws_security_group" "my_sg" {
   }
 }
 
-# Fetch latest Ubuntu AMI dynamically
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -45,11 +37,10 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# Create the EC2 instance
 resource "aws_instance" "ubuntu_vm" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key.key_name
+  key_name      = "my-key"  # reference the AWS-created key pair
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
 
